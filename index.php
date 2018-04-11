4312e36ad35e6fd00ca023d7c973ec0c4f2d2457
Damilare Daniel <danieldamilare18@gmail.com>
2018-04-11 02:57:09 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en-us">
+  <head>
+    <meta charset="utf-8">
+    <title>hotelsng</title>
+    <link href="/daniel/css/default.css" rel="stylesheet" type="text/css"/>
+  </head>
+  <body>
+    <div id="wrapper">
+      <p class="style1">Hotels.ng internship 4.0</p>
+      <p class="style2">Damilare Daniel</p>
+      <p class="style3"><?php  echo date("h:i:sa");?></p>
+    </div> 
+    
+
+  </body>
+
+</html>
